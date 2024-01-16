b2e57d3968a5f7894137efb6ff21d1f4997333ff
Manas Upadhyay <manasupadhyay1318@gmail.com>
2024-01-16 13:39:52 +0530

    final code push

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,22 +1,64 @@
-import React from "react";
-import Fullcalendar from "@fullcalendar/react";
+import React, { useRef } from "react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
+import 'react-calendar-timeline/lib/Timeline.css'
+import { userData } from "../assests/data";
+import { users } from "../assests/users";
+import FullCalendar from "@fullcalendar/react";
+
 
 function Calendar() {
+
+  const calendarRef = useRef(null);
+
+  const events = userData.layers[0].layers.map(event => {
+    const user = users.users.find(user => user.id === event.userId);
+
+    return {
+      title: `User ${user ? user.name : "Unknown"}`,
+      start: new Date(event.startDate),
+      end: new Date(event.endDate),
+    };
+  });
+
+  const handleTwoDayView = () => {
+    const currentDate = calendarRef.current.getApi().getDate();
+    const newDate = new Date(currentDate);
+    newDate.setDate(newDate.getDate() - 2);
+    calendarRef.current.getApi().gotoDate(newDate);
+  };
+
+  console.log(events);
+
   return (
     <div>
-      <Fullcalendar
+      <FullCalendar
+        ref={calendarRef}
         plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
         initialView={"dayGridMonth"}
         headerToolbar={{
-          start: "today prev,next", // will normally be on the left. if RTL, will be on the right
+          start: "today prev,next",
           center: "title",
-          end: "dayGridMonth,timeGridWeek,timeGridDay", // will normally be on the right. if RTL, will be on the left
+          end: "dayGridMonth,timeGridWeek,timeGridDay,twoDayButton", // Add the custom button
         }}
         height={"90vh"}
+        events={events}
+        customButtons={{
+          twoDayButton: {
+            text: '2 Days',
+            click: handleTwoDayView,
+          },
+        }}
+        views={{
+          twoDay: {
+            type: 'timeGrid',
+            duration: { days: 2 },
+            buttonText: '2 Days',
+          },
+        }}
       />
+
     </div>
   );
 }
